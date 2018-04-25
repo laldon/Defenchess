4e3bfa3eead0334e2cd790004e39489e49e081ff
Can Cetin <nitrocan@gmail.com>
2018-04-25 00:30:30 -0400

    exp

--- src/see.cpp
+++ src/see.cpp
@@ -38,17 +38,6 @@ int get_smallest_attacker(Position *p, Bitboard board, Color color, Square squar
     return curr_index;
 }
 
-int see(Position *p, Bitboard board, Color color, int piece_value, Square to) {
-    int val = 0;
-    int smallest_attacker = get_smallest_attacker(p, board, color, to);
-
-    if (smallest_attacker >= 0) {
-        board ^= bfi[smallest_attacker];
-        val = piece_value - see(p, board, opponent_color(color), piece_values[p->pieces[smallest_attacker]], to);
-    }
-    return val;
-}
-
 bool see_capture(Position *p, Move move) {
     Move m_type = move_type(move);
     if (m_type != NORMAL) {
@@ -58,14 +47,40 @@ bool see_capture(Position *p, Move move) {
     Bitboard board = p->board;
     Square from = move_from(move);
     Square to = move_to(move);
+    Color color = piece_color(p->pieces[from]);
     
     int piece_value = piece_values[p->pieces[to]];
     int capturer_value = piece_values[p->pieces[from]];
-    if (piece_value >= capturer_value) {
+
+    int balance = piece_value;
+    if (balance >= capturer_value) {
         return true;
     }
 
     board ^= bfi[from];
-    return piece_value >= see(p, board, piece_color(p->pieces[from]), capturer_value, to);
-}
+    bool opponent_to_move = true;
 
+    int last_attacker = capturer_value;
+    while (true) {
+        int smallest_attacker = get_smallest_attacker(p, board, color, to);
+        if (smallest_attacker == -1) {
+            break;
+        }
+
+        if (opponent_to_move)
+            balance -= piece_values[p->pieces[last_attacker]];
+        else
+            balance += piece_values[p->pieces[last_attacker]];
+
+        opponent_to_move = !opponent_to_move;
+
+        if (balance < 0) {
+            break;
+        }
+
+        color = opponent_color(color);
+        board ^= bfi[smallest_attacker];
+        last_attacker = smallest_attacker;
+    }
+    return opponent_to_move;
+}
