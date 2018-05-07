4470d03c68ac14b75f2da7c3e382135204cf5810
Can Cetin <nitrocan@gmail.com>
2018-05-06 20:05:47 -0400

    exp

--- src/eval.cpp
+++ src/eval.cpp
@@ -732,9 +732,9 @@ int evaluate(Position *p, int *opponent_king_eval) {
     eval.score_white += white_king_score;
     eval.score_black += black_king_score;
     if (p->color == white) {
-        *opponent_king_eval = (black_king_score.midgame + black_king_score.endgame) / 2;
+        *opponent_king_eval = black_king_score.midgame;
     } else {
-        *opponent_king_eval = (white_king_score.midgame + white_king_score.endgame) / 2;
+        *opponent_king_eval = white_king_score.midgame;
     }
 
     evaluate_threats(&eval, p);
