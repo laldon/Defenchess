dd0e8e1b46d4b91490802aa2a9d782fbf925a252
Can Cetin <can_cetin@icloud.com>
2018-05-20 05:17:11 +0000

    seems to work

--- src/texel.cpp
+++ src/texel.cpp
@@ -29,20 +29,17 @@ using namespace std;
 
 double value_errors[1024] = {};
 
-vector<string> fen_split(string s) {
-    vector <string> tmp;
+void fen_split(string s, vector<string> &f) {
     unsigned l_index = 0;
     for (unsigned i = 0 ; i < s.length() ; i++) {
         if (s[i] == '|') {
-            tmp.push_back(s.substr(l_index, i - l_index));
+            f.push_back(s.substr(l_index, i - l_index));
             l_index = i + 1;
         }
         if (i == s.length() - 1) {
-            tmp.push_back(s.substr(l_index));
+            f.push_back(s.substr(l_index));
         }
     }
-
-    return tmp;
 }
 
 Score
@@ -121,7 +118,8 @@ void single_error(int thread_id, double k) {
         file_mtx.lock();
         if (getline(fens, line)) {
             file_mtx.unlock();
-            vector<string> fen_info = fen_split(line);
+            vector<string> fen_info;
+            fen_split(line, fen_info);
             Position *p = import_fen(fen_info[0], thread_id);
             Metadata *md = &p->my_thread->metadatas[0];
             string result_str = fen_info[1];
@@ -150,39 +148,34 @@ void single_error(int thread_id, double k) {
     }
 }
 
-void find_error(int k) {
+double find_error(double k) {
     n = 0;
     sum = 0.0;
     fens.open("fewfens.txt");
     for (int i = 0; i < num_threads; ++i) {
         SearchThread *t = &search_threads[i];
-        t->thread_obj = std::thread(single_error, i, double(k) / 100.0);
+        t->thread_obj = std::thread(single_error, i, k);
     }
     for (int i = 0; i < num_threads; ++i) {
         SearchThread *t = &search_threads[i];
         t->thread_obj.join();
     }
     fens.close();
-    cout << "errors[" << k << "]: " << sum / double(n) << endl;
-    value_errors[k] = sum / double(n);
+    return sum / double(n);
 }
 
 void tune() {
-    double min_error = 1.0;
-    int k = 80, best = 80;
-    for(; k <= 120; ++k) {
-        find_error(k);
-    }
-    cout << "Done" << endl;
-    for (int x = 80; x <= 120; ++x) {
-        double err = value_errors[x];
-        // cout << "errors[" << x << "] = " << err << endl;
+    double min_error = 1000000.0;
+    int x = 0, best = 0;
+    for(; x <= 60; ++x) {
+        queen_check_penalty = x;
+        double err = find_error(0.96);
+        cout << "errors[" << x << "]: " << err << endl;
         if (err < min_error) {
             min_error = err;
             best = x;
         }
-
     }
-    cout << "best k: " << best << endl;
+    cout << "best queen_check_penalty: " << best << endl;
 }
 
